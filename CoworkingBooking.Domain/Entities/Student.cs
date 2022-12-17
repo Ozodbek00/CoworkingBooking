f1fbb132a0bc334de7af23818625cd1f64adaec5
akhadov <shakhriyor.akhadov@gmail.com>
2022-12-17 19:08:35 +0500

    models created

---
+++ CoworkingBooking.Domain/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using CoworkingBooking.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoworkingBooking.Domain.Entities
+{
+    public class Student : Auditable
+    {
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string PhoneNumber { get; set; } = String.Empty;
+    }
+}
