f1ad20beb1170bdcdec36eeaf9460e5e702349b9
Paxan7251 <abbosabdullayev533@gmail.com>
2022-12-18 14:04:59 +0500

    some controllers created

---
+++ CoworkingBooking.Api/Controllers/BranchesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoworkingBooking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BranchesController : ControllerBase
+    {
+        
+        [HttpGet]
+#pragma warning disable CS1998 
+        public async Task<IActionResult> GetAllAsync()
+
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(long id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(long id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(long id)
+        {
+            return Ok();
+        }
+        
+    }
+}
---
+++ CoworkingBooking.Api/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoworkingBooking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+#pragma warning disable CS1998
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(long id)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(long id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(long id)
+        {
+            return Ok();
+        }
+    }
+}
